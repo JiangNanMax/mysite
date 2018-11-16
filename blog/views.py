537194966b07b7f75ffc110a3ab2e79b5c1fc765
JiangNanMax <1321064814@qq.com>
2018-11-16 14:32:05 +0800

    实现日期归档

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,7 @@ def blog_list(request):
     context['page_range'] = page_range
     #context['blogs_count'] = Blog.objects.all().count()
     context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')
     return render_to_response('blog/blog_list.html', context)
 
 def blog_detail(request, blog_pk):
@@ -73,6 +74,41 @@ def blogs_with_type(request, blog_type_pk):
     context['page_range'] = page_range
     #context['blogs_count'] = Blog.objects.all().count()
     context['blog_types'] = BlogType.objects.all()
-
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')
 
     return render_to_response('blog/blogs_with_type.html', context)
+
+
+def blogs_with_date(request, year, month):
+    context = {}
+    blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
+
+    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER) #每5篇进行分页
+    page_num = request.GET.get('page', 1) #获取页面参数
+    page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    #!!!
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(range(current_page_num, min(paginator.num_pages, current_page_num + 2) + 1))
+
+
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')        
+
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+
+    context = {}
+    context['blogs'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
+    context['page_range'] = page_range
+    #context['blogs_count'] = Blog.objects.all().count()
+    context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')
+    context['blogs_with_date'] = '%s年%s月' % (year, month)
+
+    return render_to_response('blog/blogs_with_date.html', context)
