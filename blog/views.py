04b1f54c436f53f535a2c82c7fef444d41acebc9
JiangNanMax <1321064814@qq.com>
2018-11-20 22:57:21 +0800

    计数功能强化

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,7 @@ from django.core.paginator import Paginator
 from django.conf import settings
 from django.db.models import Count
 from .models import Blog, BlogType
-from django.contrib.contenttypes.models import ContentType
-from read_statistics.models import ReadNum
+from read_statistics.utils import read_statistics_once_read
 
 # Create your views here.
 
@@ -81,22 +80,11 @@ def blogs_with_date(request, year, month):
 def blog_detail(request, blog_pk):
     context = {}
     blog = get_object_or_404(Blog, pk=blog_pk)
-    if not request.COOKIES.get('blog_%s_readed' % blog_pk):
-        ct = ContentType.objects.get_for_model(Blog)
-        if ReadNum.objects.filter(content_type=ct, object_id=blog.pk).count():
-            #存在记录
-            readnum = ReadNum.objects.get(content_type=ct, object_id=blog.pk)
-        else:
-            #不存在对应的记录
-            readnum = ReadNum(content_type=ct, object_id=blog.pk)
-
-        readnum.read_num += 1
-        readnum.save()
-
+    read_cookie_key = read_statistics_once_read(request, blog)
 
     context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
     context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     context['blog'] = blog
     response = render_to_response('blog/blog_detail.html', context)
-    response.set_cookie('blog_%s_readed' % blog_pk, 'true')
+    response.set_cookie(read_cookie_key, 'true')
     return response
