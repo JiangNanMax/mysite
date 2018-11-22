16a23d7ca1cf1293e74cd50b073f8e49946d8897
JiangNanMax <1321064814@qq.com>
2018-11-22 19:11:27 +0800

    缓存提速

--- mysite/views.py
+++ mysite/views.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
 import datetime
 from django.db.models import Sum
+from django.core.cache import cache
 from read_statistics.utils import get_week_read_data
 from blog.models import Blog
 
@@ -17,8 +18,15 @@ def get_week_hot_data():
 def home(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
     dates, read_nums = get_week_read_data(blog_content_type)
+
+    # 获取缓存数据
+    week_hot_data = cache.get('week_hot_data')
+    if week_hot_data is None:
+        week_hot_data = get_week_hot_data()
+        cache.set('week_hot_data', week_hot_data, 3600)
+
     context = {}
     context['dates'] = dates
     context['read_nums'] = read_nums
-    context['week_hot_data'] = get_week_hot_data()
+    context['week_hot_data'] = week_hot_data
     return render(request, 'home.html', context)
