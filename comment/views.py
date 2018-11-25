a51fc9cf7d7af2eac5b4cceb75d60060fa4eafaa
JiangNanMax <1321064814@qq.com>
2018-11-25 14:36:32 +0800

    ajax提交评论内容

--- comment/views.py
+++ comment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
+from django.http import JsonResponse
 from .models import Comment
 from .forms import CommentForm
 
@@ -37,12 +38,22 @@ def update_comment(request):
 
     referer = request.META.get('HTTP_REFERER', reverse('home'))
     comment_form = CommentForm(request.POST, user=request.user)
+    data = {}
+
     if comment_form.is_valid():
         comment = Comment()
         comment.user = comment_form.cleaned_data['user']
         comment.text = comment_form.cleaned_data['text']
         comment.content_object = comment_form.cleaned_data['content_object']
         comment.save()
-        return redirect(referer)
+
+        data['status'] = 'SUCCESS'
+        data['username'] = comment.user.username
+        data['comment_time'] = comment.comment_time.strftime('%Y-%m-%d %H:%M')
+        data['text'] = comment.text
+        #return JsonResponse(data) #redirect(referer)
     else:
-        return render(request, 'error.html', {'message': comment_form.errors, 'redirect_to': referer})
+        #return render(request, 'error.html', {'message': comment_form.errors, 'redirect_to': referer})
+        data['status'] = 'ERROR'
+        data['message'] = list(comment_form.errors.values())[0][0]
+    return JsonResponse(data) #redirect(referer)
